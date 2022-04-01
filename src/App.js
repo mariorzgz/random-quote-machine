9332eb75efdd589fbeb1ad2cd83ef6a1fb86f2ba
mariorzgz <mario.rod.gonz97@gmail.com>
2022-04-01 21:25:23 +0200

    Added fonts and colors

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="title">Random Quote Generator</h1>
       <QuoteBox />
       <p className="credits">By <a href="https://github.com/mariorzgz">mariorzgz</a></p>
     </div>
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -2,24 +2,35 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import './Colors.css';
+import './Fonts.css';
 
 const colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
-];
+                "bg-pink",
+                "bg-purple",
+                "bg-orange",
+                "bg-turquose",
+                "bg-blue",
+                "bg-darkgreen"
+                ];
+
+const fonts = [
+                "serif",
+                "sans-serif",
+                "monospace",
+                "handwriting"
+              ];
 
 const getRandomQuote = (data) => {
   return(data.quotes[Math.floor(Math.random() * data.quotes.length)])
+}
+
+const RandomColor = () =>{
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
+const RandomFont = () =>{
+  return fonts[Math.floor(Math.random() * fonts.length)]
 }
 
 class QuoteBox extends React.Component {
@@ -27,7 +38,9 @@ class QuoteBox extends React.Component {
     super(props);
     this.state = {
       quote : "",
-      author: ""
+      author: "",
+      color: "",
+      font: "",
     };
   }
 
@@ -43,28 +56,31 @@ class QuoteBox extends React.Component {
       let quote = getRandomQuote(jsonResult)
       this.setState({
         quote: quote.quote,
-        author: quote.author
+        author: quote.author,
+        color: RandomColor(),
+        font: RandomFont()
       })
     })
   }
 
   render() {
+    document.getElementById("root").className = this.state.color;
     return <div id="quote-box">
-      <div className="quote">
-        <div id="text">
+      <div className={`quote ${this.state.font}`}>
+        <div id="text" className={this.state.font}>
           <h2><FontAwesomeIcon icon={faQuoteLeft} />{this.state.quote}</h2>
         </div>
         <div id="author">— {this.state.author}</div>
       </div>
       <div className="buttons">
-        <a id="tweet-quote"
+        <a className={this.state.color} id="tweet-quote"
           href={`https://twitter.com/intent/tweet?text="${this.state.quote}"%20by%20${this.state.author}&hashtags=RandomQuoteMachine,MiPrimeraAppEnReact`}
           data-size="large"
           target="_blank"
           rel="noreferrer">
           <FontAwesomeIcon icon={faTwitter}/>
         </a>
-        <button id ="new-quote" onClick={this.getQuote.bind(this)}>
+        <button id="new-quote" className={this.state.color} onClick={this.getQuote.bind(this)}>
           New Quote
         </button>
       </div>
