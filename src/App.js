750d1c9adaf07aed3907961ddf26e827a0bf048b
mariorzgz <mario.rod.gonz97@gmail.com>
2022-04-01 16:54:26 +0200

    Added style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <QuoteBox />
+      <p className="credits">By <a href="https://github.com/mariorzgz">mariorzgz</a></p>
     </div>
   );
 }
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -3,6 +3,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
+const colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+];
 
 const getRandomQuote = (data) => {
   return(data.quotes[Math.floor(Math.random() * data.quotes.length)])
@@ -31,26 +45,29 @@ class QuoteBox extends React.Component {
         quote: quote.quote,
         author: quote.author
       })
-  })
+    })
   }
 
   render() {
     return <div id="quote-box">
-      <div id="text">
-        <FontAwesomeIcon icon={faQuoteLeft} />{this.state.quote}
+      <div className="quote">
+        <div id="text">
+          <h2><FontAwesomeIcon icon={faQuoteLeft} />{this.state.quote}</h2>
+        </div>
+        <div id="author">— {this.state.author}</div>
       </div>
-      <div id="author">By {this.state.author}</div>
-      <button id ="new-quote" onClick={this.getQuote.bind(this)}>
-        New Quote
-      </button>
-      <a id="tweet-quote"
-         href={`https://twitter.com/intent/tweet?text="${this.state.quote}"%20by%20${this.state.author}&hashtags=RandomQuoteMachine,MiPrimeraAppEnReact`}
-         data-size="large"
-         target="_blank"
-         rel="noreferrer">
-         <FontAwesomeIcon icon={faTwitter}/>
-      Tweet
-      </a>
+      <div className="buttons">
+        <a id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text="${this.state.quote}"%20by%20${this.state.author}&hashtags=RandomQuoteMachine,MiPrimeraAppEnReact`}
+          data-size="large"
+          target="_blank"
+          rel="noreferrer">
+          <FontAwesomeIcon icon={faTwitter}/>
+        </a>
+        <button id ="new-quote" onClick={this.getQuote.bind(this)}>
+          New Quote
+        </button>
+      </div>
     </div>;
   }
 }
